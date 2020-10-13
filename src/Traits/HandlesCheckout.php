e24dd88afe139169f04acc192f02f4ac79c06c3c
Clouduser <genesis@fastfwd.com>
2020-10-13 10:37:20 +0100

    Postcode Validation

--- src/Traits/HandlesCheckout.php
+++ src/Traits/HandlesCheckout.php
@@ -203,7 +203,82 @@ trait HandlesCheckout
             'address_3' => 'nullable|string',
             'address_city' => 'required|string',
             'address_county' => 'nullable|string',
-            'address_postcode' => 'required|string',
+            'address_postcode' => [
+                'required',
+                'string',
+                function ($attribute, $value, $fail) {
+                    $alpha1 = "[abcdefghijklmnoprstuwyz]";                          // Character 1
+                    $alpha2 = "[abcdefghklmnopqrstuvwxy]";                          // Character 2
+                    $alpha3 = "[abcdefghjkstuw]";                                   // Character 3
+                    $alpha4 = "[abehmnprvwxy]";                                     // Character 4
+                    $alpha5 = "[abdefghjlnpqrstuwxyz]";                             // Character 5
+                    
+                    // Expression for postcodes: AN NAA, ANN NAA, AAN NAA, and AANN NAA with a space
+                    // Or AN, ANN, AAN, AANN with no whitespace
+                    $pcexp[0] = '^(' . $alpha1 . '{1}' . $alpha2 . '{0,1}[0-9]{1,2})([[:space:]]{0,})([0-9]{1}' . $alpha5 . '{2})?$';
+                    
+                    // Expression for postcodes: ANA NAA
+                    // Or ANA with no whitespace
+                    $pcexp[1] = '^(' . $alpha1 . '{1}[0-9]{1}' . $alpha3 . '{1})([[:space:]]{0,})([0-9]{1}' . $alpha5 . '{2})?$';
+                    
+                    // Expression for postcodes: AANA NAA
+                    // Or AANA With no whitespace
+                    $pcexp[2] = '^(' . $alpha1 . '{1}' . $alpha2 . '[0-9]{1}' . $alpha4 . ')([[:space:]]{0,})([0-9]{1}' . $alpha5 . '{2})?$';
+                    
+                    // Exception for the special postcode GIR 0AA
+                    // Or just GIR
+                    $pcexp[3] = '^(gir)([[:space:]]{0,})?(0aa)?$';
+                    
+                    // Standard BFPO numbers
+                    $pcexp[4] = '^(bfpo)([[:space:]]{0,})([0-9]{1,4})$';
+                    
+                    // c/o BFPO numbers
+                    $pcexp[5] = '^(bfpo)([[:space:]]{0,})(c\/o([[:space:]]{0,})[0-9]{1,3})$';
+                    
+                    // Overseas Territories
+                    $pcexp[6] = '^([a-z]{4})([[:space:]]{0,})(1zz)$';
+                    
+                    // Anquilla
+                    $pcexp[7] = '^(ai\-2640)$';
+                    
+                    // Load up the string to check, converting into lowercase
+                    $postcode = strtolower($value);
+                    
+                    // Assume we are not going to find a valid postcode
+                    $valid = false;
+                    
+                    // Check the string against the six types of postcodes
+                    foreach ($pcexp as $regexp) {
+                        if (preg_match('/' . $regexp . '/i', $postcode, $matches)) {
+                    
+                        // Load new postcode back into the form element
+                        $postcode = strtoupper($matches[1]);
+                        if (isset($matches[3])) {
+                            $postcode .= ' ' . strtoupper($matches[3]);
+                        }
+                    
+                        // Take account of the special BFPO c/o format
+                        $postcode = preg_replace('/C\/O/', 'c/o ', $postcode);
+                    
+                        // Remember that we have found that the code is valid and break from loop
+                        $valid = true;
+                        break;
+                        }
+                    }
+
+                    if (preg_match('/\s{2,}/', $value)) {
+                        $valid = false;
+                    }
+                    
+                    // Return with the reformatted valid postcode in uppercase if the postcode was 
+                    // valid
+                    if ($valid) {
+                        $value = $postcode;
+                    } else {
+                        $fail($attribute.' is invalid.');
+                    }
+                }
+            ],
             'address_country' => 'nullable|string',
         ])->validate();
     }
